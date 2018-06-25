8520bf629364b24d84b07f0644b1cbd97896acba
Jude Lawson <lawsonjdutton@gmail.com>
2018-06-25 17:35:40 -0600

    Tests and implements transaction model validations

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    it { should validate_presence_of(:credit_card_number) }
+    it { should validate_presence_of(:result) }
+    # We could need this, but the CSV data is all empty right now
+    # Awaiting clarification
+    # it { should validate_presence_of(:credit_card_expiration_date) }
+  end
 end
