5579968e7111fbbcf2d430ed855ee984f467999c
Jude Lawson <lawsonjdutton@gmail.com>
2018-06-25 17:45:23 -0600

    Tests and implements validations for Customer model. Also cleans up transactions_spec.

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Transaction, type: :model do
+RSpec.describe Transaction do
   describe 'Validations' do
     it { should validate_presence_of(:credit_card_number) }
     it { should validate_presence_of(:result) }
