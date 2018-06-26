e33da3f720b0101da3b8283bffad719e18a01417
Jude Lawson <lawsonjdutton@gmail.com>
2018-06-26 08:23:02 -0600

    Implementing search_results class method IS WIP

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -1,14 +1,7 @@
 class Api::V1::Merchants::SearchController < ApplicationController
   def show
-    if search_params.keys.first == 'created_at'
-      parsed_created_date = Date.parse(search_params['created_at'])
-      render json: Merchant.where(created_at: (parsed_created_date.beginning_of_day..parsed_created_date.end_of_day)).limit(1).first
-    elsif search_params.keys.first == 'updated_at'
-      parsed_created_date = Date.parse(search_params['updated_at'])
-      render json: Merchant.where(updated_at: (parsed_created_date.beginning_of_day..parsed_created_date.end_of_day)).limit(1).first
-    else
-      render json: Merchant.find_by(search_params)
-    end
+
+    render json: Merchant.search_result(search_params)
   end
 
   private
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -4,4 +4,24 @@ RSpec.describe Merchant do
   describe 'Validations' do
     it { should validate_presence_of(:name) }
   end
+
+  describe 'Class Methods' do
+    describe '.search_result' do
+      it 'should return a single record based on given key value pair' do
+        merchant = Merchant.create!(name: 'Merchant 1')
+        search_params = { name: 'Merchant 1'}
+        expect(Merchant.search_result(search_params)).to eq(merchant)
+      end
+    end
+
+    describe '.search_results' do
+      it 'should return a collection of results based on provided search params' do
+        merchant1 = Merchant.create!(name: 'Merchant 1')
+        merchant2 = Merchant.create!(name: 'Merchant 2')
+
+        search_params = { created_at: merchant1.created_at }
+        expect(Merchant.search_results(search_params).length).to eq(2) 
+      end
+    end
+  end
 end
