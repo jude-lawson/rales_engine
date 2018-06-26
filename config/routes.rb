897c96792921c334e4f9e6c3b89b73e51510c393
Judson Stevens <Judson.Stevens@gmail.com>
2018-06-26 17:36:01 -0600

    Transaction relationship and test

--- config/routes.rb
+++ config/routes.rb
@@ -34,7 +34,9 @@ Rails.application.routes.draw do
       get 'transactions/find', to: 'transactions/search#show'
       get 'transactions/find_all', to: 'transactions/search#index'
       get 'transactions/random', to: 'transactions/random#show'
-      resources :transactions, only: [:index, :show]
+      resources :transactions, only: [:index, :show] do
+        get 'invoice', to: 'transactions/invoices#show'
+      end
 
       # Customers
       get 'customers/find', to: 'customers/search#show'
