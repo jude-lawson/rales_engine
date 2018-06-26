018de6faac117ce60f15456cd2e429934221b8d2
Jude Lawson <lawsonjdutton@gmail.com>
2018-06-26 13:43:24 -0600

    Fixes misnomer parsed_created_date local variable from serach_result and search_results method in ApplicationRecord

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -4,8 +4,8 @@ class ApplicationRecord < ActiveRecord::Base
   def self.search_result(search_params)
     param = search_params.keys.first
     if param == 'created_at' || param == 'updated_at'
-      parsed_created_date = Date.parse(search_params[param])
-      where(param => (parsed_created_date.beginning_of_day..parsed_created_date.end_of_day)).limit(1).first
+      parsed_date = Date.parse(search_params[param])
+      where(param => (parsed_date.beginning_of_day..parsed_date.end_of_day)).limit(1).first
     else
       find_by(search_params)
     end
@@ -14,8 +14,8 @@ class ApplicationRecord < ActiveRecord::Base
   def self.search_results(search_params)
     param = search_params.keys.first
     if param == 'created_at' || param == 'updated_at'
-      parsed_created_date = Date.parse(search_params[param])
-      where(param => (parsed_created_date.beginning_of_day..parsed_created_date.end_of_day))
+      parsed_date = Date.parse(search_params[param])
+      where(param => (parsed_date.beginning_of_day..parsed_date.end_of_day))
     else
       where(search_params)
     end
